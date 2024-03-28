19ad94b5f18c966138003ae758bf130f26687423
NamSangMin <skatkdals5@gmail.com>
2024-03-28 17:37:25 +0900

    second commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,20 +4,22 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <div className='header'>
-      <h1>
-        <Link to="/">토익 영단어(고급)</Link>
-      </h1>
-    <div className='menu'>
-        <Link to="/create_word" className='link'>
-            단어 추가
-        </Link>
-        <Link to="/create_day" className='link'>
-            Day 추가
-        </Link>
-        <Link to="/delete_day" className='link'>
-            Day 삭제
-        </Link>
-    </div>
+      <span className='home_name' >
+        <h1>
+          <Link to="/">영단어</Link>
+        </h1>
+      </span>
+      <span className='menu'>
+          <Link to="/create_word" className='link'>
+              단어 추가
+          </Link>
+          <Link to="/create_day" className='link'>
+              Day 추가
+          </Link>
+          <Link to="/delete_day" className='link'>
+              Day 삭제
+          </Link>
+      </span>
     </div>
   );
 }
